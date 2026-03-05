ade90bae0ad593a24b31850e1c2db696f6fd512b
BharatSiva <saiuppalapati16@gmail.com>
2026-03-05 10:12:00 +0530

    UC4: Character Array Based Palindrome Check

--- APP/src/PalindroneCheckerApp.java
+++ APP/src/PalindroneCheckerApp.java
@@ -7,18 +7,19 @@ public class PalindroneCheckerApp {
         System.out.print("Input text: ");
         String input = sc.nextLine();
 
-        String reversed = "";
+        boolean isPalindrome = true;
+        int j = 0;
 
         // Iterate from the last character to the first
         for (int i = input.length() - 1; i >= 0; i--) {
-            reversed += input.charAt(i);
+            if (input.charAt(i) != input.charAt(j)) {
+                isPalindrome = false;
+                break;
+            }
+            j++;
         }
 
-        boolean isPalindrome = input.equals(reversed);
-
         System.out.println("Is it a Palindrome? : " + isPalindrome);
-
         sc.close();
     }
 }
-
